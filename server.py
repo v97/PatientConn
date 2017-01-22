11bdf507c6fc9b49f5bec4f4072af8afa14e2b51
v97 <vsambamurthy@connect.ust.hk>
2017-01-22 04:58:20 +0000

    hopefully everythings done

--- server.py
+++ server.py
@@ -29,37 +29,68 @@ def newPatientJSON(name, age, male, symptoms):
     patient['prescriptions'] = prescription
     return patient
 
-@app.route('/addPatient/', [methods = 'POST'])
-def addPatient(name, age, male, symptoms = None):
-	#<name>/<age>/<male>/<symptoms>
-	a = request.form()
-	#if symptoms is None:
-	#	symptoms = {}
-	#id = patients.insert(newPatientJSON(name, age, male, symptoms))
-	print(a)
-	return "134"
+@app.route('/addPatient/', methods = ['POST'])
+def addPatient():
+	symptoms = request.form["symptoms"]
+	if symptoms is None:
+		symptoms = {}
+	id = patients.insert(newPatientJSON(request.form["name"], request.form["age"], request.form["male"], symptoms))
+	return str(id)
 
-@app.route('/addSymptomInstance')
-def addSymptomInstance(id, symptom, freq, severity, start, end = None):
-    patient = patients.find({"_id": id}).next()
+@app.route('/updatePatient/', methods = ['POST'])
+def updatePatient():
+	patients.update({"_id": ObjectId(request.form["id"])}, request.form["patient"], upsert=True)
+	return True
+
+@app.route('/updateNutrition/', methods = ['POST'])
+def updateNutrition():
+	patient = patients.find({"_id": ObjectId(id)}).next()
+	patient["prescription"]["nutrition"] = request.form["nutrition"]
+	patients.update({"_id": ObjectId(request.form["id"])}, patient, upsert=True)
+	return True
+
+@app.route('/updateExercise/', methods = ['POST'])
+def updateExercise():
+	patient = patients.find({"_id": ObjectId(id)}).next()
+	patient["prescription"]["exercise"] = request.form["exercise"]
+	patients.update({"_id": ObjectId(request.form["id"])}, patient, upsert=True)
+	return True
+
+@app.route('/updateSleep/', methods = ['POST'])
+def updateSleep():
+	patient = patients.find({"_id": ObjectId(id)}).next()
+	patient["prescription"]["sleep"] = request.form["sleep"]
+	patients.update({"_id": ObjectId(request.form["id"])}, patient, upsert=True)
+	return True
+
+def TEMP_ADD(name, age, male, symptoms = None):
+	if symptoms is None:
+		symptoms = {}
+	id = patients.insert(newPatientJSON(name, age, male, symptoms))
+	return str(id)
+
+@app.route('/addSymptomInstance/<id>/<symptom>/<freq>/<severity>/<start>/<end>/')
+def addSymptomInstance(id, symptom, freq, severity, start, end):
+    patient = patients.find({"_id": ObjectId(id)}).next()
     instance = {"start": start, "end": end, "freq": freq, "severity": severity}
     if symptom in patient["symptoms"]:
         patient["symptoms"][symptom]["instances"].append(instance)
     else:
         patient["symptoms"][symptom] = {"instances": [instance]}
-    patients.update({"_id": id}, patient, upsert=True)
+    patients.update({"_id": ObjectId(id)}, patient, upsert=True)
 
-@app.route('/endOngoingSymptom/<symptom>/<end>')
-def endOngoingSymptom(id, symptom, end):
-	patient = patients.find({"_id": id}).next()
-	patient["symptoms"][symptom]["instances"][-1]["end"] = end
-	patients.update({"_id": id}, patient, upsert=True)
-
+@app.route('/symptomOngoing/<id>/<symptom>')
 def symptomOngoing(id, symptom):
-    patient = patients.find({"_id": id}).next()
+    patient = patients.find({"_id": ObjectId(id)}).next()
     if symptom in patient["symptoms"] == False:
         return False
     return (patient["symptoms"][symptom]["instances"][-1]["end"] is None) == False
+
+@app.route('/endOngoingSymptom/<symptom>/<end>')
+def endOngoingSymptom(id, symptom, end):
+	patient = patients.find({"_id": ObjectId(id)}).next()
+	patient["symptoms"][symptom]["instances"][-1]["end"] = end
+	patients.update({"_id": ObjectId(id)}, patient, upsert=True)
     
 def getDate(date):
     temp = date.split('-')
@@ -69,36 +100,54 @@ def getDate(date):
     date['Y'] = int('20' + temp[2]) if temp[2] < 100 else int(temp[2])
     return date
 
+@app.route('/addMedicine/<drug>/<dosage>/<freq>/<comments>/<start>/<end>')
 def addMedicine(drug, dosage, freq, comments, start, end):
-    patient = patients.find({"_id": id})
-    instance = {"dosage": dosage, "freq": freq, "start": start, "end": end, "comments": comments}
-    patient["prescription"]["medication"][drug] = patient["prescription"]["medication"].get("drug", []) + [instance]
-    patients.update({"_id": id}, patient, upsert=True)
+	patient = patients.find({"_id": ObjectId(id)}).next()
+	instance = {"dosage": dosage, "freq": freq, "start": start, "end": end, "comments": comments}
+	patient["prescription"]["medication"][drug] = patient["prescription"]["medication"].get("drug", []) + [instance]
+	patients.update({"_id": ObjectId(id)}, patient, upsert=True)
+	return True
 
-def endOngoingMedicine(drug, end):
-    patient = patients.find({"_id": id})
-    patient["prescription"]["medication"][drug][-1]["end"] = end
-    patients.update({"_id": id}, patient, upsert=True)
+@app.route('/endOngoingMedicine/<id>/<drug>/<end>')
+def endOngoingMedicine(id, drug, end):
+	patient = patients.find({"_id": ObjectId(id)}).next()
+	patient["prescription"]["medication"][drug][-1]["end"] = end
+	patients.update({"_id": ObjectId(id)}, patient, upsert=True)
+	return True
 
+@app.route('/medicineOngoing/<id>/<drug>')
 def medicineOngoing(id, drug):
-    patient = patients.find({"_id": id}).next()
+    patient = patients.find({"_id": ObjectId(id)}).next()
     if drug in patient["prescription"]["medication"] == False:
         return False
     return (patient["prescription"]["medication"][drug][-1]["end"] is None) == False
 
-def getMedication():
-    patient = patients.find({"_id": id})
-    patient["medication"]
-    patients.update({"_id": id}, patient, upsert=True)
+@app.route('/getMedication/<id>')
+def getMedication(id):
+    patient = patients.find({"_id": ObjectId(id)}).next()
+    return patient["medication"]
 
-patients.delete_many({})
-id = addPatient("Vikram", 19, True)	
-print(id)
+@app.route('/getNutrition/<id>')
+def getNutrition(id):
+    patient = patients.find({"_id": ObjectId(id)}).next()
+    return patient["nutrition"]
 
 @app.route('/getPatient/<id>')
 def getPatient(id):
 	return str(patients.find({"_id": ObjectId(id)}).next())
 
+@app.route('/getPatients/')
+def getPatients():
+	return str([docs for docs in patients.find({})])
+
+@app.route('/clear/')
+def clear():
+	patients.delete_many({})
+	return True
+
+patients.delete_many({})
+TEMP_ADD("Vikram", 19, True)
+TEMP_ADD("Kshit", 29, False)	
 #addSymptomInstance(id, "Jew", True, 10, 1, "21/01/2017")
 #endOngoingSymptom(id, "Jew", "22/01/2026")
 app.run()
